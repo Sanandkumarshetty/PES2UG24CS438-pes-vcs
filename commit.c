f5aff1834681712a7d8f4cfea1ddbf70ae9d4cf6
Sanandkumarshetty <sanandkumarshetty@gmail.com>
2026-04-16 10:59:13 +0000

    improvised commit_create function in commit.c

--- commit.c
+++ commit.c
@@ -214,6 +214,7 @@ int head_update(const ObjectID *new_commit) {
 //   - head_update       : moves the branch pointer to your new commit
 //
 // Returns 0 on success, -1 on error.
+
 int commit_create(const char *message, ObjectID *commit_id_out) {
     if (!message || !commit_id_out) return -1;
 
